efb2163992dd1b0dc73397050a708e12120ce4fb
Digvijay Girase <digvijaygd@goldengekko.com>
2015-02-26 19:24:31 +0530

    test

--- app/src/main/java/com/dmi/minesafety/demo/InspectionOptionsActivity.java
+++ app/src/main/java/com/dmi/minesafety/demo/InspectionOptionsActivity.java
@@ -30,6 +30,8 @@ public class InspectionOptionsActivity extends Activity {
 
     private String[] mLocationsTitles;
 
+    private int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
